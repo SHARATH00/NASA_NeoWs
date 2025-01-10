d31351e8c1f30aebeab2fe78a76d024ee9e5a214
Sharath Dopathi <dopathisharathreddy1122@gmail.com>
2025-01-10 09:34:05 -0500

    made few chnages to the file

--- scripts/logger.py
+++ scripts/logger.py
@@ -1,10 +1,12 @@
 import logging
 
-def logger():
+def get_logger():
     logging.basicConfig(
-        filename="logs/pipeline.log",
+        filename="scripts/logs/pipeline.log",
         filemode="a",
         format="%(asctime)s - %(levelname)s - %(message)s",
         level=logging.INFO
     )
-    return logging.getLogger()
+    return logging.getLogger('neo_logger')
+
+logger = get_logger()
