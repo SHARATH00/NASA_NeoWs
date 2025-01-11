5d4e57b622675fb5af04466d723f63986c4ada49
Sharath Dopathi <dopathisharathreddy1122@gmail.com>
2025-01-11 09:20:04 -0500

    made changes to a log command

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -10,15 +10,18 @@ def fetch_all_neo_data():
     url = f"{BASE_URL}?api_key={API_KEY}"
     all_data = []  # List to store all results
 
-   # while url:
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        data = response.json()
-        all_data.extend(data['near_earth_objects'])  # Append the current page's data
-        url = data.get('links', {}).get('next')  # Move to the next page if available
-        logger.info(f"Fetched page: {BASE_URL}")
-    except requests.RequestException as e:
-        logger.error(f"Error fetching data from NASA API: {e}")
-    #break
+    while url:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            data = response.json()
+            all_data.extend(data['near_earth_objects'])  # Append the current page's data
+            url = data.get('links', {}).get('next')  # Move to the next page if available
+            # Log the current URL without the API key
+            log_url = url.split('&api_key=')[0]
+            logger.info(f"Fetched page: {log_url}")
+            #logger.info(f"Fetched page: {BASE_URL}")
+        except requests.RequestException as e:
+            logger.error(f"Error fetching data from NASA API: {e}")
+            break
     return all_data
