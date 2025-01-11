2a12a0bbde7c376c6a86ce7f14f14e50cba0f049
Sharath Dopathi <dopathisharathreddy1122@gmail.com>
2025-01-10 21:33:04 -0500

    made big change so that asteroid and close_approaches have all required column data

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,3 @@
-# main.py
 from fetch_data import fetch_all_neo_data
 from database import connect_db, upsert_data
 from data_checks import validate_api_response
@@ -8,23 +7,46 @@ def main():
     try:
         # Fetch data
         data = fetch_all_neo_data()
-       # validate_api_response(data)
+        # validate_api_response(data)
 
         # Process data
         asteroids = []
         close_approaches = []
         for obj in data:
-            #for obj in objects:
-                # Process asteroid data (example logic)
-                asteroid = ( 
-                    obj['id'], obj['name'], obj['neo_reference_id'], obj['absolute_magnitude_h']
-                )
-                asteroids.append(asteroid)
+            # Process asteroid data
+            asteroid = (
+                obj['id'], obj['name'], obj['neo_reference_id'], obj['absolute_magnitude_h'],
+                obj['estimated_diameter']['kilometers']['estimated_diameter_min'],
+                obj['estimated_diameter']['kilometers']['estimated_diameter_max'],
+                (obj['estimated_diameter']['kilometers']['estimated_diameter_min'] + obj['estimated_diameter']['kilometers']['estimated_diameter_max']) / 2,
+                obj['is_potentially_hazardous_asteroid'], obj['orbital_data']['orbit_id'],
+                obj['orbital_data']['orbit_determination_date'], obj['orbital_data']['first_observation_date'],
+                obj['orbital_data']['last_observation_date'], obj['orbital_data']['data_arc_in_days'],
+                obj['orbital_data']['observations_used'], obj['orbital_data']['orbit_uncertainty'],
+                obj['orbital_data']['minimum_orbit_intersection'], obj['orbital_data']['jupiter_tisserand_invariant'],
+                obj['orbital_data']['epoch_osculation'], obj['orbital_data']['eccentricity'],
+                obj['orbital_data']['semi_major_axis'], obj['orbital_data']['inclination'],
+                obj['orbital_data']['ascending_node_longitude'], obj['orbital_data']['orbital_period'],
+                obj['orbital_data']['perihelion_distance'], obj['orbital_data']['perihelion_argument'],
+                obj['orbital_data']['aphelion_distance'], obj['orbital_data']['perihelion_time'],
+                obj['orbital_data']['mean_anomaly'], obj['orbital_data']['mean_motion'],
+                obj['orbital_data']['equinox'], obj['orbital_data']['orbit_class']['orbit_class_type'],
+                obj['orbital_data']['orbit_class']['orbit_class_description'], obj['orbital_data']['orbit_class']['orbit_class_range'],
+                obj['is_sentry_object']
+            )
+            asteroids.append(asteroid)
 
-                # Process close approach data
-                for approach in obj['close_approach_data']:
-                    close_approach = (obj['id'], approach['close_approach_date'])
-                    close_approaches.append(close_approach)
+            # Process close approach data
+            for approach in obj['close_approach_data']:
+                close_approach = (
+                    obj['id'], approach['close_approach_date'], approach['close_approach_date_full'],
+                    approach['epoch_date_close_approach'], approach['relative_velocity']['kilometers_per_second'],
+                    approach['relative_velocity']['kilometers_per_hour'], approach['relative_velocity']['miles_per_hour'],
+                    approach['miss_distance']['astronomical'], approach['miss_distance']['lunar'],
+                    approach['miss_distance']['kilometers'], approach['miss_distance']['miles'],
+                    approach['orbiting_body']
+                )
+                close_approaches.append(close_approach)
 
         # Save to database
         conn = connect_db()
