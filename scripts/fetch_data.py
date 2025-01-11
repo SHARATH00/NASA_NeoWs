181c5174be9ad311d8569108b9656b6d5d879d0b
Sharath Dopathi <dopathisharathreddy1122@gmail.com>
2025-01-10 21:34:13 -0500

    made a change to a logger command which could show potential information like API key in the log files

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -17,7 +17,7 @@ def fetch_all_neo_data():
         data = response.json()
         all_data.extend(data['near_earth_objects'])  # Append the current page's data
         url = data.get('links', {}).get('next')  # Move to the next page if available
-        logger.info(f"Fetched page: {url}")
+        logger.info(f"Fetched page: {BASE_URL}")
     except requests.RequestException as e:
         logger.error(f"Error fetching data from NASA API: {e}")
     #break
